7c03d3ff38290122dcb4370207cb37810bfbd5fe
Hyeonjun Park <phjppo0918@gmail.com>
2023-09-12 14:51:29 +0900

    fin

---
+++ step2/usefile/actions.js
@@ -0,0 +1,6 @@
+function over(obj) {
+    obj.src="../pororo.jpg";
+}
+function out(obj) {
+    obj.src="../chrong.jpg";
+}
---
+++ step3/action.js
@@ -0,0 +1,3 @@
+var today = new Date();
+
+document.write("현재 시간 :" + today.toLocaleDateString() + today.toLocaleTimeString())
